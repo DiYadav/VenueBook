1dc98a6bd6725a16afe39dde6aac4b3cf2526c3c
Dinesh <yadavdinesh2852002@gmail.com>
2025-12-16 21:37:37 +0530

    database created

--- backend/Venue/settings.py
+++ backend/Venue/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'venuebook',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
